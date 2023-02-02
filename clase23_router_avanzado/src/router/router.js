400aaceb95450b814a36b4a2ec00a1ff56272ceb
Arturo Verbel <arturo.verbel@bia.app>
2023-02-01 21:11:07 -0400

    Clase 23 - Router Avanzado (HandsOnLab Manejo de Policies)

--- clase23_router_avanzado/src/router/router.js
+++ clase23_router_avanzado/src/router/router.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import jwt from 'jsonwebtoken'
 
 export default class MyRouter {
 
@@ -13,20 +14,20 @@ export default class MyRouter {
 
     init(){}
 
-    get(path, ...callbacks){
-        this.router.get(path, this.generateCustomResponses, this.applyCallbacks(callbacks) )
+    get(path, policies, ...callbacks){
+        this.router.get(path, this.generateCustomResponses, this.handlePolicies(policies), this.applyCallbacks(callbacks) )
     }
 
-    post(path, ...callbacks){
-        this.router.post(path, this.generateCustomResponses, this.applyCallbacks(callbacks) )
+    post(path, policies, ...callbacks){
+        this.router.post(path, this.generateCustomResponses, this.handlePolicies(policies), this.applyCallbacks(callbacks) )
     }
 
-    put(path, ...callbacks){
-        this.router.put(path, this.generateCustomResponses, this.applyCallbacks(callbacks) )
+    put(path, policies, ...callbacks){
+        this.router.put(path, this.generateCustomResponses, this.handlePolicies(policies), this.applyCallbacks(callbacks) )
     }
 
-    delete(path, ...callbacks){
-        this.router.delete(path, this.generateCustomResponses, this.applyCallbacks(callbacks) )
+    delete(path, policies, ...callbacks){
+        this.router.delete(path, this.generateCustomResponses, this.handlePolicies(policies), this.applyCallbacks(callbacks) )
     }
 
     applyCallbacks(callbacks) {
@@ -48,7 +49,32 @@ export default class MyRouter {
         res.sendSuccess = payload => res.send({status: "success", payload})
         res.sendServerError = error => res.status(500).send({status: "error", error })
         res.sendUserError = error => res.status(400).send({status: "error", error})
+        res.sendNoAuthenticatedError = error => res.status(401).send({status: "error", error})
+        res.sendNoAuthorizatedError = error => res.status(403).send({status: "error", error})
         
+        next()
+    }
+
+    handlePolicies = policies => (req, res, next) => {
+        if(policies.includes('PUBLIC')) return next()
+
+        if(policies.length > 0) {
+            const authHeaders = req.headers.authorization
+            if(!authHeaders) return res.sendNoAuthenticatedError('Unauthenticated')
+
+            const tokenArray = authHeaders.split(" ")
+            const token = (tokenArray.length > 1) ? tokenArray[1] : tokenArray[0]
+
+            const user = jwt.verify(token, 'secret')
+
+            if(!policies.includes(user.role.toUpperCase()) ) {
+                return res.sendNoAuthorizatedError("Unauthorizated")
+            }
+
+            req.user = user
+            return next()
+        }
+
         next()
     }
 
--- clase23_router_avanzado/src/router/user.router.js
+++ clase23_router_avanzado/src/router/user.router.js
@@ -1,18 +1,32 @@
 import MyRouter from './router.js'
+import jwt from 'jsonwebtoken'
 
 export default class UsersRouter extends MyRouter {
 
     init() {
-        this.get('/', (req, res) => {
+        this.post('/login', ["PUBLIC"], (req, res) => {
+            const user = {
+                email: req.query.email,
+                role: "user"
+            }
+
+            const token = jwt.sign(user, "secret")
+            res.sendSuccess( { token } )
+        })
+
+        this.get('/', ["PUBLIC"], (req, res) => {
             res.sendSuccess('HOla a todos')
         })
 
-        this.get('/:word', (req, res) => {
+        this.get('/:word', ["USER"], (req, res) => {
             if(req.params.word == 'error') {
                 return res.sendServerError("Word incorrect")
             }
             
-            return res.sendSuccess(req.params.word)
+            return res.sendSuccess({
+                word: req.params.word,
+                user: req.user
+            })
         })
     }
 
